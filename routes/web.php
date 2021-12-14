193ea9a26603dbe9e0bed0cb741e0afd6eb953c0
sajjat43 <mdsajjat43@gmail.com>
2021-12-14 19:35:34 +0600

    backend template integration done

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('master');
+});
+
+
+//for admin controller
+Route::get('/form/signup', [AdminController::class, 'Signup']);
