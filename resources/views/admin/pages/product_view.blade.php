63b6195a88915e4463f9b455a790540f5ed41e90
sajjat43 <mdsajjat43@gmail.com>
2022-02-27 16:35:36 +0600

    edit

--- resources/views/admin/pages/product_view.blade.php
+++ resources/views/admin/pages/product_view.blade.php
@@ -1,16 +1,17 @@
 @extends('master')
 @section('content')
     <div class="container-xl">
-            <div class="card" style="width: 30rem; margin-left: 25%">
-                <div class="card-body">
-                  <h5 class="card-title">Product Details</h5>
-                  <img src="{{url('/uploads/uploads/product/'.$product->image)}}" style="border-radius:4px" margin-left="20%" height="200px" width="200px" alt="product image">
-                  <p><b>Product Name: {{$product->name}}</b></p>
-                  <p><b>Product Catagory Name: {{$product->product_category}}</b></p>
-                  <p><b>Product Price: {{$product->price}}</b></p>
-                  <p><b>Product Description: {{$product->description}}</b></p>
+        <div class="card" style="width: 30rem; margin-left: 25%">
+            <div class="card-body">
+                <h5 class="card-title">Product Details</h5>
+                <img src="{{ url('/uploads/uploads/product/' . $product->image) }}" style="border-radius:4px"
+                    margin-left="20%" height="200px" width="200px" alt="product image">
+                <p><b>Product Name: {{ $product->name }}</b></p>
+                <p><b>Product Catagory Name: {{ $product->product_category }}</b></p>
+                <p><b>Product Price: {{ $product->price }}</b></p>
+                <p><b>Product Description: {{ $product->description }}</b></p>
 
-                </div>
-              </div>
+            </div>
+        </div>
     </div>
 @endsection
