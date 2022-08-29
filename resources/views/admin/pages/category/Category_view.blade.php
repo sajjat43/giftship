ab1a1154cb3223d5d7916acc29bbb5c1eec8e713
sajjat43 <mdsajjat43@gmail.com>
2022-08-29 23:37:11 +0600

    print button add in category subctegory

--- resources/views/admin/pages/category/Category_view.blade.php
+++ resources/views/admin/pages/category/Category_view.blade.php
@@ -17,8 +17,12 @@
             </ul>
         </div>
     @endif
-   
+    <div class="col-xl-3 float-end m-3">
+        <button class="btn btn-primary text-capitalize border-0" data-mdb-ripple-color="dark"
+            onclick="printDiv('printableArea')"><i class="fas fa-print text-primary"></i> Print</button>
+    </div>
     {{-- <a href="{{route('product.create')}}" class="btn btn-primary"> Add product</a> --}}
+    <div class="container" id="printableArea">
     <table class=" table table-light" style="width: 100%">
         <h1 class="text-center py-3">All Category</h1>
         <thead>
@@ -42,8 +46,7 @@
                         <a href="{{ route('product.category.update.view', $data->id) }}" class="btn btn-info"><i
                                 class="fa-solid fa-pen-to-square"></i></a>
 
-{{-- 
-                        <a href="{{ route('category.delete', $data->id) }}" class="btn btn-success">Delete</a> --}}
+                      {{--<a href="{{ route('category.delete', $data->id) }}" class="btn btn-success">Delete</a> --}}
                         {{-- <a href="{{route('product.category.delete', $data->Cid)}}" class="btn btn-danger">Delete</a> --}}
                     </td>
                 </tr>
@@ -51,7 +54,18 @@
 
         </tbody>
     </table>
-
-
+  </div>
+    <script>
+        function printDiv(printableArea) {
+                    var printContents = document.getElementById(printableArea).innerHTML;
+                    var originalContents = document.body.innerHTML;
+        
+                    document.body.innerHTML = printContents;
+        
+                    window.print();
+        
+                    document.body.innerHTML = originalContents;
+                }
+    </script>
 
 @endsection
--- resources/views/admin/pages/sub_category/subCategory_view.blade.php
+++ resources/views/admin/pages/sub_category/subCategory_view.blade.php
@@ -17,16 +17,20 @@
     </ul>
 </div>
 @endif
-
+<div class="col-xl-3 float-end m-3">
+    <button class="btn btn-primary text-capitalize border-0" data-mdb-ripple-color="dark"
+        onclick="printDiv('printableArea')"><i class="fas fa-print text-primary"></i> Print</button>
+</div>
 {{-- <a href="{{route('product.create')}}" class="btn btn-primary"> Add product</a> --}}
-<table class=" table table-light " style="width: 100%">
+<div class="container" id="printableArea">
+<table class=" table table-light "  style="width: 100%">
     <h1 class="py-5">All SubCategory List</h1>
     <thead>
         <tr>
             <th scope="col">ID</th>
             <th scope="col">subCatagory name</th>
             <th scope="col">subCategory description</th>
-            <th scope="col">category name</th>
+            {{-- <th scope="col">category name</th> --}}
             <th scope="col">image</th>
             <th scope="col">Action</th>
         </tr>
@@ -37,7 +41,7 @@
             <th scope="row">{{ $key + 1 }}</th>
             <td>{{ $data->subname }} </td>
             <td>{{ $data->subdescription }} </td>
-            <td>{{ $data->category->Cname }} </td>
+            {{-- <td>{{ $data->category->Cname }} </td> --}}
             <td><img src="{{ url('uploads/uploads/subcategory/' . $data->subimage) }}" style="border-radius:4px"
                     width="100px" alt="Category image"></td>
             <td>
@@ -51,7 +55,18 @@
 
     </tbody>
 </table>
-
-
+</div>
+<script>
+    function printDiv(printableArea) {
+                var printContents = document.getElementById(printableArea).innerHTML;
+                var originalContents = document.body.innerHTML;
+    
+                document.body.innerHTML = printContents;
+    
+                window.print();
+    
+                document.body.innerHTML = originalContents;
+            }
+</script>
 
 @endsection
