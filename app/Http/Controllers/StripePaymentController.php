633e2feb802f377c83f7eafc647abc7a071ea5bc
sajjat43 <mdsajjat43@gmail.com>
2023-01-09 08:30:30 +0600

    strip payment null error

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Stripe\Charge;
 use Stripe\Stripe;
+use App\Models\order;
+use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\RequestDetails;
+use App\Models\RequestProduct;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 
 class StripePaymentController extends Controller
@@ -29,7 +35,78 @@ class StripePaymentController extends Controller
                 "source" => $request->stripeToken,
                 "description" => "Test payment from ofdem.com." 
         ]);
-  
+        try{
+            $order = order::create([
+                'user_id' => auth()->user()->id,
+                // 'tran_id' => $post_data['tran_id'],
+                'discount' =>(session()->get('coupon')['discount']),
+                'payment_method'=>'SSL',
+                'name' => $request->name,
+                'email' => $request->email,
+                'mobile' => $request->mobile,
+                'Address' => $request->Address,
+                'total' =>array_sum(array_column(session()->get('cart'),'subtotal'))+50-(session()->get('coupon')['discount']),
+                
+            ]);
+        
+    }catch (\Throwable $th){
+        $order = order::create([
+            'user_id' => auth()->user()->id,
+            // 'tran_id' => $post_data['tran_id'],
+            'payment_method'=>'SSL',
+            'name' => $request->name,
+            'email' => $request->email,
+            'mobile' => $request->mobile,
+            'Address' => $request->Address,
+            'total' =>array_sum(array_column(session()->get('cart'),'subtotal'))+50,
+            
+        ]);
+    }
+            
+            $carts = session()->get('cart');
+            if ($carts) {
+                $total = 0;
+                $request = RequestProduct::create([
+                    'user_id' => auth()->user()->id,
+    
+                ]);
+                foreach ($carts as $key => $cart) {
+                    RequestDetails::create([
+    
+                        'user_id' => auth()->user()->id,
+                        'order_id'=>$order->id,
+                        'request_id' => $request->id,
+                        // 'tran_id' => $post_data['tran_id'],
+                        'product_id' => $cart['product_id'],
+                        'quantity' => $cart['product_qty'],
+                        'price' =>$cart['product_price'],
+                        'sub_total' => $cart['product_price'] * $cart['product_qty'],
+                        // 'total_price' => $total += $cart['product_price'] * $cart['product_qty'],
+                    ]);
+                    $product = Product::find($key);
+                    $product->decrement('qty', $cart['product_qty']);
+                }
+    
+                $token = Str::random(64);
+                $newOrder=Order::where('id',$order->id)->with('RequestDetails','RequestDetails.product')->first();
+                Mail::send('website.email.orderConfirm',compact('newOrder'), function ($message) use ($request) {
+                    $message->to(auth()->user()->email);
+                    $message->subject('order confirm'); 
+                });
+    
+                // if($session=session()->has('coupon')){
+                //     $session=session()->get('coupon')['name'];
+                // dd($session);
+                // $coupon=Coupon::where('code',$session)->first();
+                // $coupon->update([
+                //     'expiry_date'=>'2022-08-23',
+                // ]);
+                // }
+                session()->forget('cart');
+                session()->forget('coupon');
+                // $coupon=Coupon::find($id);
+                // return redirect(route('manage.home'))->with('message', 'request placed Successfully');
+            }
         Session::flash('success', 'Payment successful!');
           
         return back();
