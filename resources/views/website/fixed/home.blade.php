a3fe52c92294a0ab86e052ff5e0b33dda1c0b314
sajjat43 <mdsajjat43@gmail.com>
2023-01-23 17:17:41 +0600

    top section space reduce

--- resources/views/website/fixed/home.blade.php
+++ resources/views/website/fixed/home.blade.php
@@ -21,7 +21,9 @@
     <div class="slider-area ">
         <div class="slider-active">
             <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
-                <div class="container">
+                <div style=" 
+                    margin-top: -52px;
+                " class="container">
                     <div class="row d-flex align-items-center justify-content-between">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
                             <div class="hero__img" data-animation="bounceIn" data-delay=".4s">
@@ -176,7 +178,7 @@
             background-color: red;
             color: rgb(255, 0, 0);
         }
-
+      
     </style>
 
 
